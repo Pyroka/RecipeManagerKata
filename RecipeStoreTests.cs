bfbdc74937a034f3a241780775f37cbdcd9cf515
Andrew Thorpe <pyroka@gmail.com>
2014-08-16 14:39:46 +0100

    'GetAllRecipes_AfterRecipeAdded_ReturnsSingleRecipe' changed to 'GetAllRecipes_AfterRecipesAdded_ReturnsRecipes' to prove that multiple recipes are handled. Implemented CreateRecipe helper function for the test.

--- RecipeStoreTests.cs
+++ RecipeStoreTests.cs
@@ -7,6 +7,18 @@ namespace RecipeManager
     [TestClass]
     public class RecipeStoreTests
     {
+        private static Recipe CreateRecipe(string name, int size, string text)
+        {
+            return new Recipe
+            {
+                Name = name,
+                Size = size,
+                Text = text
+            };
+        }
+
+        // --------------------------------------------------------------------
+
         [TestMethod]
         public void GetAllRecipies_WithNoRecipies_ReturnsEmpty()
         {
@@ -18,21 +30,18 @@ namespace RecipeManager
         }
 
         [TestMethod]
-        public void GetAllRecipies_AfterRecipeAdded_ReturnsSingleRecipe()
+        public void GetAllRecipies_AfterRecipesAdded_ReturnsRecipes()
         {
             var store = new InMemoryRecipeStore();
-            store.AddRecipe("TestRecipe", "Put the lime in the cocanut");
+            store.AddRecipe("TestRecipe1", "Put the lime in the cocanut");
+            store.AddRecipe("TestRecipe2", "Get green eggs, add ham");
 
             var result = store.GetAllRecipies();
 
             var expected = new[]
             {
-                new Recipe
-                {
-                    Name = "TestRecipe",
-                    Size = 27,
-                    Text = "Put the lime in the cocanut"
-                }
+                CreateRecipe("TestRecipe1", 27, "Put the lime in the cocanut"),
+                CreateRecipe("TestRecipe2", 23, "Get green eggs, add ham")
             };
             result.ShouldAllBeEquivalentTo(expected);
         }
