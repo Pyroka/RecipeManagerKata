1893532dd6e0b1f48da9a471f730b60dd13518a3
Andrew Thorpe <pyroka@gmail.com>
2014-08-16 19:31:08 +0100

    FileSystemRecipeStore now uses the RootDirectory that's passed in to the constructor.

--- FileSystemRecipeStore.cs
+++ FileSystemRecipeStore.cs
@@ -6,24 +6,27 @@ namespace RecipeManager
 {
     public class FileSystemRecipeStore : IRecipeStore
     {
+        private string RootDirectory { get; set; }
+
         public FileSystemRecipeStore(string rootDirectory)
         {
+            RootDirectory = rootDirectory;
         }
 
         public IEnumerable<Recipe> GetAllRecipies()
         {
-            return new DirectoryInfo(@"e:\portkata").GetFiles("*")
+            return new DirectoryInfo(RootDirectory).GetFiles("*")
                 .Select(fileInfo => new Recipe { Name = fileInfo.Name, Size = fileInfo.Length, Text = File.ReadAllText(fileInfo.FullName) });
         }
 
         public void DeleteRecipeNamed(string name)
         {
-            File.Delete(@"e:\portkata\" + name);
+            File.Delete(Path.Combine(RootDirectory, name));
         }
 
         public void SaveRecipe(string name, string directions)
         {
-            File.WriteAllText(Path.Combine("e:\\portkata", name), directions);
+            File.WriteAllText(Path.Combine(RootDirectory, name), directions);
         }
     }
 }
